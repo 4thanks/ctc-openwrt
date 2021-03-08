81db35c22bfb6d515d11290b12194a59269afa36
4thanks <63891627+4thanks@users.noreply.github.com>
2021-03-08 18:30:01 +0800

    Create customize.sh

---
+++ customize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: kenzo
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.50.2/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 自定义设置
+#cp -f package/litte/default-settings package/lean/default-settings/files/zzz-default-settings
+#cp -f package/litte/banner package/base-files/files/etc/banner
+#cp -f package/litte/Leandiffconfig diffconfig && cp diffconfig .config && make defconfig
+#./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a
+
