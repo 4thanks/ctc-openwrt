372a7bd64545da476ff073697789656ff311833d
4thanks <63891627+4thanks@users.noreply.github.com>
2022-07-12 22:29:35 +0800

    Create update-mosdns-conf.sh

--- diy/update-mosdns-conf.sh
+++ diy/update-mosdns-conf.sh
@@ -1,3 +1,12 @@
 # get conf yaml
+mkdir -p /tmp/confyaml
+#sbwml-conf
+curl -sS https://raw.githubusercontent.com/sbwml/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/config_custom.yaml > /tmp/confyaml/sbwml-conf.conf
 
-mkdir -p /tmp/confyaml && curl https://raw.githubusercontent.com/sbwml/luci-app-mosdns/master/luci-app-mosdns/root/etc/mosdns/config_custom.yaml > /tmp/confyaml/sbwml-conf.yaml && curl https://raw.githubusercontent.com/pmkol/easymosdns/main/config.yaml > /tmp/confyaml/easymosdns-conf.yaml && curl https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/config.yaml > /tmp/confyaml/hezhijie0327-conf.yaml && \cp -rf /tmp/confyaml/*.yaml /diy/mosdns && rm -rf /tmp/confyaml/*
+#easymosdns-conf
+curl -sS https://raw.githubusercontent.com/pmkol/easymosdns/main/config.yaml > /tmp/confyaml/easymosdns-conf.conf
+
+#easymosdns-conf
+curl -sS https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/config.yaml > /tmp/confyaml/hezhijie0327-conf.conf
+
+cp -rf /tmp/confyaml/*.yaml /diy/mosdns/ && rm -rf /tmp/confyaml/*
