5fb64b87000fe5d65df434cb1f4620dd326b942b
4thanks <63891627+4thanks@users.noreply.github.com>
2022-07-14 16:24:00 +0800

    Update update-mosdns-conf.sh

--- diy/update-mosdns-conf.sh
+++ diy/update-mosdns-conf.sh
@@ -10,8 +10,12 @@ curl -sS https://raw.githubusercontent.com/pmkol/easymosdns/main/config.yaml > /
 curl -sS https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/config.yaml > /tmp/hezhijie0327-conf.yaml
 
 cp -rf /tmp/*.yaml diy/mosdns/
+rm -rf /tmp/*
 
 #rules && Geosite GeoIP && cndbIP data
 curl -sS https://raw.githubusercontent.com/hezhijie0327/CNIPDb/main/cnipdb/country_ipv4_6.dat > /tmp/GeoIP_CNIPDb.dat
+curl https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat > /tmp/geosite.dat
+curl https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat > /tmp/geoip.dat
 
-cp -f /tmp/GeoIP_CNIPDb.dat diy/mosdns/data/GeoIP_CNIPDb.dat
+cp -rf /tmp/*.dat diy/mosdns/
+rm -rf /tmp/*
