7c5f5b08b848ab8835481178b6166098f8a494d9
4thanks <63891627+4thanks@users.noreply.github.com>
2022-07-14 17:51:09 +0800

    Update update-mosdns-conf.sh

--- diy/update-mosdns-conf.sh
+++ diy/update-mosdns-conf.sh
@@ -10,14 +10,12 @@ curl -sS https://raw.githubusercontent.com/pmkol/easymosdns/main/config.yaml > /
 curl -sS https://raw.githubusercontent.com/hezhijie0327/CMA_DNS/main/mosdns/config.yaml > /tmp/hezhijie0327-conf.yaml
 
 cp -rf /tmp/*.yaml diy/mosdns/
+rm -rf /tmp/*
 
 #rules && Geosite GeoIP && cndbIP data
-curl -sS https://raw.githubusercontent.com/hezhijie0327/CNIPDb/main/cnipdb/country_ipv4_6.dat > /tmp/GeoIP_CNIPDb.dat
+curl -sS https://raw.githubusercontent.com/hezhijie0327/CNIPDb/main/cnipdb/country_ipv4_6.dat > /tmp/datGeoIP_CNIPDb.dat
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat > /tmp/geosite.dat
 curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat > /tmp/geoip.dat
 
-# cp -f /tmp/GeoIP_CNIPDb.dat diy/mosdns/GeoIP_CNIPDb.dat
-# cp -f /tmp/geosite.dat diy/mosdns/geosite.dat
-# cp -f /tmp/geoip.dat diy/mosdns/geoip.dat
-cp -rf /tmp/*.dat diy/mosdns/
+cp -rf /tmp/*.dat diy/mosdns/data/
 rm -rf /tmp/*
